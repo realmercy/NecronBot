db8a15a442ed9728e1d08b5fea1f1e62b031a9b1
realmercy <90040892+realmercy@users.noreply.github.com>
2022-04-08 18:22:57 +0530

    Update avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -1,21 +1,21 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
 const { MessageEmbed } = require("discord.js");
-module.exports = {
-  data: new SlashCommandBuilder()
-    .setName("avatar")
-    .setDescription("get a user's or your own avatar")
-    .addUserOption((option) =>
-      option.setName("user").setDescription("user whose avatar you want to see")
-    ),
-  async execute(interaction) {
-    const user = interaction.options.getUser("user") || interaction.user.tag;
-    const embed = new MessageEmbed()
-      .setImage(`${user.displayAvatarURL({ dynamic: true, size: 4096 })}`)
-      .setTimestamp()
-      .setColor(`${user.hexAccentColor}`)
-      .setTitle(`${user.username}'s avatar`)
-      .setURL(`${user.displayAvatarURL({ dynamic: true, size: 4096 })}`)
-      .setFooter(`${interaction.user.tag}`);
-    return interaction.reply({ embeds: [embed] });
-  },
-};
+module.exports = { data: new SlashCommandBuilder() 
+.setName("avatar") 
+.setDescription("get a user's or your own avatar") 
+.addUserOption((option) => option .setName("user") 
+.setDescription("user whose avatar you want to see") ), 
+async execute(interaction) { const user = interaction.options.getUser("user"); 
+if (user) { const embed = new MessageEmbed() .setImage(`${user.displayAvatarURL({ dynamic: true, size: 4096 })}`) 
+.setTimestamp() .setColor("RANDOM") 
+.setTitle(`${user.username}'s avatar`) 
+.setURL(`${user.displayAvatarURL({ dynamic: true, size: 4096 })}`) 
+.setFooter(`${interaction.user.tag}`); 
+return interaction.reply({ embeds: [embed] }); } 
+else { const embed = new MessageEmbed() 
+.setImage( `${interaction.user.displayAvatarURL({ dynamic: true, size: 4096 })}` ) 
+.setTimestamp() 
+.setColor("RANDOM") 
+.setTitle(`${interaction.user.username}'s avatar`) 
+.setURL( `${interaction.user.displayAvatarURL({ dynamic: true, size: 4096 })}` ); 
+return interaction.reply({ embeds: [embed] }); } },};
